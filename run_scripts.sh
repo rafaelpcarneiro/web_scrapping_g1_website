437f28ebb8169ad026691afc5d46c7f7e63e1fb6
rafaelpcarneiro <rafaelpcarneiro@gmail.com>
2022-05-22 00:06:54 -0300

    run_scripts.sh is not repeating unnecessary commands now

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,11 +1,23 @@
 #/bin/bash
 
+md5sum g1database.db > checksum.md5_g1database
+
 ./collect_data.sh
 
-# Plot data
-echo ""
-echo "Creating Plots ..."
-./plot_topics.py
-./plot_wordcloud.py
+md5sum -c checksum.md5_g1database --status
 
-echo "Plots created"
+# Plot data
+if [ $? -eq 1 ]
+then
+    echo "g1database was updated"
+    echo ""
+    echo "Creating Plots ..."
+    ./plot_topics.py
+    ./plot_wordcloud.py
+
+    echo "Plots created"
+else
+    echo "g1database was not updated."
+    echo "There is no need to run plot scripts now"
+fi
+rm checksum.md5_g1database
